5a50f0d6429bb3bdadc050eb47f3aa1fa5cadb7f
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2025-01-12 23:56:45 +0900

    Update SpriteShader.hlsl

--- Resources/Shaders/SpriteShader.hlsl
+++ Resources/Shaders/SpriteShader.hlsl
@@ -28,38 +28,38 @@ struct VS_IN
 {
     float3 pos : POSITION;
     float4 color : COLOR;
-    float2 TexCoord : TEXCOORD;
+    float2 uv : TEXCOORD;
 };
 
 struct VS_OUT
 {
-    float4 position : SV_POSITION;
+    float4 pos : SV_POSITION;
     float4 color : COLOR;
-    float2 TexCoord : TEXCOORD;
+    float2 uv : TEXCOORD;
 };
 
 VS_OUT VS_Main(VS_IN input)
 {
-    VS_OUT result = (VS_OUT) 0;
+    VS_OUT output = (VS_OUT) 0;
     
     float2 scale = g_Scale;
     float2 offset = (g_Pos / g_window_size);
     float2 pos = input.pos.xy * g_Scale + offset;
-    result.position = float4(pos.xy * float2(2, -2) + float2(-1, 1), g_depth, 1);
+    output.pos = float4(pos.xy * float2(2, -2) + float2(-1, 1), g_depth, 1);
  
     float2 tex_scale = (g_texSampleSize/ g_origintexSize);
     float2 tex_offset = (g_texSamplePos / g_origintexSize);
     
-    result.TexCoord = input.TexCoord * tex_scale + tex_offset;
+    output.uv = input.uv * tex_scale + tex_offset;
     
-    result.color = input.color;
+    output.color = input.color;
     
-    return result;
+    return output;
 }
 
 
 float4 PS_Main(VS_OUT input) : SV_TARGET
 {
-    float4 texColor = texDiffuse.Sample(samplerDiffuse, input.TexCoord);
-    return texColor/* * input.color*/;
+    float4 texColor = texDiffuse.Sample(samplerDiffuse, input.uv);
+    return texColor * input.color;
 }
