6e3aaf11371d4e441396c3c9d1e54c5bf68c4918
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2025-02-02 16:12:32 +0900

    텍스쳐수정.

--- Resources/Shaders/seatest.hlsl
+++ Resources/Shaders/seatest.hlsl
@@ -72,7 +72,7 @@ VS_IN WaveGeneration(VS_IN input)
     float2 waveDirections[waveCount] =
     {
         normalize(float2(1.0f, 0.2f)),
-        normalize(float2(0.0f, 1.0f)),
+        normalize(float2(0.3f, -1.0f)),
         normalize(float2(-0.5f, 0.7f))
     };
 
