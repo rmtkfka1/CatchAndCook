6f11c456a3cf71916c5f73fd051c66a1db0b0fa6
Clrain <jin_hyeon_seo_01@naver.com>
2025-02-04 04:33:40 +0900

    1

--- ResourcesUpload/Resources/Shaders/test.hlsl
+++ ResourcesUpload/Resources/Shaders/test.hlsl
@@ -1,9 +1,5 @@
 #include "GLOBAL.hlsl"
-
-struct struturedParam
-{
-    float4 color;
-};
+#include "Light.hlsl"
 
 cbuffer test : register(b1)
 {
@@ -31,19 +27,20 @@ struct VS_IN
 {
     float3 pos : POSITION;
     float2 uv : TEXCOORD0;
-    //float3 normal : NORMAL;
-    uint vertexID : SV_VertexID;
+    float3 normal : NORMAL;
+
 };
 
 struct VS_OUT
 {
     float4 pos : SV_Position;
+    float4 worldPos : POSITIONT;
+    float4 worldNormal : NORMAL;
     float2 uv : TEXCOORD0;
-    float4 paramColor : COLOR;
+
 };
 
 Texture2D g_tex_0 : register(t0);
-StructuredBuffer<struturedParam> Structured : register(t1);
 SamplerState g_sam_0 : register(s0);
 
 VS_OUT VS_Main(VS_IN input)
@@ -51,9 +48,12 @@ VS_OUT VS_Main(VS_IN input)
     VS_OUT output = (VS_OUT) 0;
 
     output.pos = mul(float4(input.pos, 1.0f), WorldMat);
-    output.pos = mul(output.pos, VPMatrix);
-    output.paramColor = Structured[input.vertexID].color;
     
+    output.worldPos = output.pos;
+    output.worldNormal = mul(float4(input.normal, 0.0f), WorldMat);
+    
+    output.pos = mul(output.pos, VPMatrix);
+
     output.uv = input.uv;
 
     return output;
@@ -61,12 +61,31 @@ VS_OUT VS_Main(VS_IN input)
 
 float4 PS_Main(VS_OUT input) : SV_Target
 {
-    float4 color = g_tex_0.Sample(g_sam_0, input.uv);
 
-    // 버텍스 셰이더에서 전달된 색상 더하기
-    color += input.paramColor;
+    float3 color;
+    
+    float4 worldPos = input.worldPos;
+    float4 WolrdNormal = input.worldNormal;
+    float3 toEye = normalize(g_eyeWorld - worldPos.xyz);
 
-    return color;
+  
+    for (int i = 0; i < g_lightCount; ++i)
+    {
+        if (g_lights[i].mateiral.lightType == 0)
+        {
+            color += ComputeDirectionalLight(g_lights[i], g_lights[i].mateiral, WolrdNormal.xyz, toEye);
+        }
+        else if (g_lights[i].mateiral.lightType == 1)
+        {
+            color += ComputePointLight(g_lights[i], g_lights[i].mateiral, worldPos.xyz, WolrdNormal.xyz, toEye);
+        }
+        else if (g_lights[i].mateiral.lightType == 2)
+        {
+            color += ComputeSpotLight(g_lights[i], g_lights[i].mateiral, worldPos.xyz, WolrdNormal.xyz, toEye);
+        }   
+    }
+
+    return float4(color, 1.0f) * g_tex_0.Sample(g_sam_0, input.uv);
     
 
 }
