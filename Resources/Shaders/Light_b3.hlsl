e029f82479c58dc06b63e483184ba7d7d87635f5
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2025-02-22 18:21:52 +0900

    Update Light_b3.hlsl

--- Resources/Shaders/Light_b3.hlsl
+++ Resources/Shaders/Light_b3.hlsl
@@ -55,13 +55,13 @@ float CalcAttenuation(float d, float falloffStart, float falloffEnd)
     return saturate((falloffEnd - d) / (falloffEnd - falloffStart));
 }
 
-//float3 BlinnPhong(float3 lightStrength, float3 lightVec, float3 normal, float3 toEye, LightMateiral mat)
-//{
-//    float3 halfway = normalize(toEye + lightVec);
-//    float hdotn = dot(halfway, normal);
-//    float3 specular = mat.specular * pow(max(hdotn,0.0f), mat.shininess);
-//    return mat.ambient + (mat.diffuse + specular) * lightStrength;
-//} 
+float3 BlinnPhong(float3 lightStrength, float3 lightVec, float3 normal, float3 toEye, LightMateiral mat)
+{
+    float3 halfway = normalize(toEye + lightVec);
+    float hdotn = dot(halfway, normal);
+    float3 specular = mat.specular * pow(max(hdotn, 0.0f), mat.shininess);
+    return mat.ambient + (mat.diffuse + specular) * lightStrength;
+}
 
 float3 Phong(float3 lightStrength, float3 lightVec, float3 normal, float3 toEye, LightMateiral mat)
 {
