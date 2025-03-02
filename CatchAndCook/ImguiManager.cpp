d5a8c68333275ab11b69a42c2cd77cc902bcc3ea
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2025-03-03 01:55:06 +0900

    imgui업데이트

--- CatchAndCook/ImguiManager.cpp
+++ CatchAndCook/ImguiManager.cpp
@@ -55,6 +55,9 @@ void ImguiManager::Render()
 			ImGui::SliderFloat3("LightDirection", &direction.x, -1.0f, 1.0f);
 			LightManager::main->_lightParmas.light[0].direction = direction;
 
+			ImGui::SliderFloat3("DiffuseColor", &LightManager::main->_lightParmas.light[0].material.diffuse.x, 0, 1.0f);
+			ImGui::SliderFloat3("AmbientColor", &LightManager::main->_lightParmas.light[0].material.ambient.x, 0, 1.0f);
+			ImGui::SliderFloat3("SpecularColor", &LightManager::main->_lightParmas.light[0].material.specular.x, 0, 1.0f);
 		}
 
 	
