2b5ce0d283f2e9edebc8a6ee4c96adcb35d376b0
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2025-04-05 23:55:44 +0900

    스탬프 개발중.

--- CatchAndCook/PathStamp.cpp
+++ CatchAndCook/PathStamp.cpp
@@ -13,38 +13,99 @@ void PathStamp::Init()
 
 void PathStamp::Run()
 {
+
 	if (Input::main->GetKeyDown(KeyCode::Enter))
 	{
 		vec3 cameraPos = CameraManager::main->GetActiveCamera()->GetCameraPos();
-		lines.push_back(cameraPos);
+		_controllPoint.push_back(cameraPos);
 	}
 
+	const int segmentCount = 30;
+	Gizmo::main->Width(3.0f);
 
-	for (int i = 0; i < lines.size(); ++i)
+	_lineData.clear();
+
+	for (int i = 0; i + 2 < _controllPoint.size(); i += 2)
 	{
-		//1개 
-		//i ==0 => lines[0] lines[0] 
+		vec3 p0 = _controllPoint[i];
+		vec3 p1 = _controllPoint[i + 1];
+		vec3 p2 = _controllPoint[i + 2];
 
-		//2개
-		//i == 0 => lines[0] lines[1]
-		//i == 1 => lines[1] lines[0]
+		for (int j = 0; j < segmentCount; ++j)
+		{
+			float t1 = (float)j / segmentCount;
+			float t2 = (float)(j + 1) / segmentCount;
 
-		//3개
-		//i == 0 => lines[0] lines[1]
-		//i == 1 => lines[1] lines[2]
-		//i == 2 => lines[2] lines[0]
+			vec3 a = BezierQuadratic(p0, p1, p2, t1);
+			vec3 b = BezierQuadratic(p0, p1, p2, t2);
 
-		//4개
-		//i == 0 => lines[0] lines[1]
-		//i == 1 => lines[1] lines[2]
-		//i == 2 => lines[2] lines[3]
-		//i == 3 => lines[3] lines[0]
+			_lineData.push_back(a);
+			_lineData.push_back(b);
 
-		vec3 from = lines[i];
-		vec3 to = lines[(i + 1) % lines.size()];
-		Gizmo::main->Width(3.0f);
-		Gizmo::main->Line(from, to, vec4(1, 0, 0, 1));
+			Gizmo::main->Line(a, b, vec4(1, 0, 0, 1));
+		}
 	}
+
+	if (Input::main->GetKeyDown(KeyCode::End))
+	{
+		if (_lineData.size() >= 2)
+		{
+			vec3 first = _lineData.front();
+			vec3 last = _lineData.back();
+
+			_lineData.push_back(last);
+			_lineData.push_back(first);
+		}
+	}
+
+
+	if (Input::main->GetKeyDown(KeyCode::Delete))
+	{
+		if (!_controllPoint.empty())
+		{
+			_controllPoint.pop_back();
+		}
+	}
+
+	if (Input::main->GetKeyDown(KeyCode::Insert))
+	{
+		std::ofstream file("BezierLines.txt");
+
+		if (!file.is_open())
+		{
+			std::cerr << "베지어 파일 저장 실패" << std::endl;
+			return;
+		}
+
+		for (const auto& point : _lineData)
+		{
+			file << point.x << " " << point.y << " " << point.z << std::endl;
+		}
+
+		file.close();
+	}
+
+
+
+
+
 
 }
+
+vec3 PathStamp::BezierQuadratic(vec3& p0, vec3& p1, vec3& p2, float& t)
+{
+	return (1 - t) * (1 - t) * p0 +
+		2 * (1 - t) * t * p1 +
+		t * t * p2;
+}
+
+vec3 PathStamp::BezierCubic(const vec3& p0, const vec3& p1, const vec3& p2, const vec3& p3, float& t)
+{
+	float u = 1 - t;
+	return u * u * u * p0 +
+		3 * u * u * t * p1 +
+		3 * u * t * t * p2 +
+		t * t * t * p3;
+}
+
 
--- CatchAndCook/PathStamp.h
+++ CatchAndCook/PathStamp.h
@@ -11,7 +11,13 @@ public:
 
 	void Init();
 	void Run();
+private:
+	vec3 BezierQuadratic(vec3& p0, vec3& p1, vec3& p2, float& t);
+	vec3 BezierCubic(const vec3& p0, const vec3& p1, const vec3& p2, const vec3& p3, float& t);
 
-	vector<vec3> lines;
+	vector<vec3> _controllPoint;
+	vector<vec3> _lineData;
+
+	const int _segmentCount = 30;
 };
 
