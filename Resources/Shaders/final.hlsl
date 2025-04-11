01e3a802806aae6abcb7300daee4db46aab1db4a
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2025-04-12 01:40:46 +0900

    원상복구

--- Resources/Shaders/final.hlsl
+++ Resources/Shaders/final.hlsl
@@ -33,5 +33,5 @@ float4 PS_Main(VS_OUT input) : SV_Target
 
     float3 lightColor = ComputeLightColor(worldPos.xyz, WolrdNormal.xyz);
     
-    return /*float4(lightColor, 1.0f) **/ AlbedoColor;
+    return float4(lightColor, 1.0f) * AlbedoColor;
 };
