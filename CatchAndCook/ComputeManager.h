99e7eea104be2039a56de0325110db6b1396c0cb
Clrain <jin_hyeon_seo_01@naver.com>
2025-04-11 15:40:23 +0900

    수치 조정

--- CatchAndCook/ComputeManager.h
+++ CatchAndCook/ComputeManager.h
@@ -84,7 +84,7 @@ private:
 	void Blooming(ComPtr<ID3D12GraphicsCommandList>& cmdList, int x, int y, int z);
 
 private:
-	int32 _blurCount = 8;
+	int32 _blurCount = 6;
 	bool _on = false;
 	shared_ptr<Texture> _bloomTexture;
 	shared_ptr<Texture> _pingtexture;
