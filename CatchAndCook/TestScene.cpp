c573c3f86fa68f3ed78d8d4f261b0ebd0f897415
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2025-05-16 03:48:41 +0900

    블렌드 테스트완료 = > 컴퓨트쉐이더 꺼두었음

--- CatchAndCook/TestScene.cpp
+++ CatchAndCook/TestScene.cpp
@@ -83,25 +83,25 @@ void TestScene::Init()
 
 
 	shared_ptr<Material> materialO = make_shared<Material>();
-	materialO->SetPass(RENDER_PASS::Forward);
+	materialO->SetPass(RENDER_PASS::Deferred);
 	shared_ptr<Mesh> mesh = GeoMetryHelper::LoadRectangleBox(1.0f);
 
 	ShaderInfo info;
 	info._blendEnable = true;
-	info.renderTargetCount = 1;
-	info.RTVForamts[0] = DXGI_FORMAT_R8G8B8A8_UNORM;
-
-	for (int i = 0; i < 8; ++i)
-	{
-		info._blendType[i] = BlendType::Add;
-	}
-
+	info.renderTargetCount = 4;
+	info.RTVForamts[0] = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	info.RTVForamts[1] = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	info.RTVForamts[2] = DXGI_FORMAT_R8G8B8A8_UNORM;
+	info.RTVForamts[3] = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	info._blendType[0] = BlendType::Add;
+	info._blendType[1] = BlendType::Add;
+	info._blendType[2] = BlendType::Add;
+	info._blendType[3] = BlendType::Add;
 
 	shared_ptr<Shader> shader = make_shared<Shader>();
-	shader->Init(L"UiForward.hlsl", StaticProp, ShaderArg{},info);
+	shader->Init(L"DeferredSea.hlsl", StaticProp, ShaderArg{},info);
 	shader->SetInjector({ BufferType::SeaDefaultMaterialParam });
 	shader->SetPass(RENDER_PASS::Deferred);
-
 
 	for (int i = 0; i < 1000; ++i)
 	{
