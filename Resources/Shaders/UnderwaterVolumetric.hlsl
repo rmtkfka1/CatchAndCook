89082cd4a6194b676c5042b4413f2bfa96fd4aed
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2025-05-17 20:06:23 +0900

    2

--- Resources/Shaders/UnderwaterVolumetric.hlsl
+++ Resources/Shaders/UnderwaterVolumetric.hlsl
@@ -21,64 +21,46 @@ cbuffer VolumetricData : register(b5)
 struct VS_OUT
 {
     float4 pos : SV_POSITION;
-    float2 uv : TEXCOORD0;
+    float3 ndcPos : POSITION;
 };
 
-VS_OUT VS_Main(uint vid : SV_VertexID)
+VS_OUT VS_Main(uint vid : SV_VertexID, uint instanceID : SV_InstanceID)
 {
     VS_OUT o;
 
     float2 verts[4] =
     {
-        float2(-1, 1), // top-left
-        float2(1, 1), // top-right
-        float2(-1, -1), // bottom-left
-        float2(1, -1) 
+        float2(-1, 1), float2(1, 1), 
+        float2(-1, -1), float2(1, -1)
     };
-
-    float2 uvs[4] =
-    {
-        float2(0, 0),
-        float2(1, 0),
-        float2(0, 1),
-        float2(1, 1)
-    };
+    
 
     o.pos = float4(verts[vid], 0.0f, 1.0f);
-    o.uv = uvs[vid];
+    float ndcZ = (instanceID + 0.5f) / numSlices;
+    o.ndcPos = float3(o.pos.xy, ndcZ);
 
+    
     return o;
 }
 
 
-float3 ProjToView(float2 uvCoord)
+float3 ProjToView(float3 ndcPos)
 {
     float4 posProj;
-    posProj.xy = uvCoord * 2.0f - 1.0f;
-    posProj.y *= -1;
-    posProj.z = 1.0f;
-    posProj.w = 1.0f;
+    
+    posProj.xy = ndcPos.xy;
+    posProj.z  = ndcPos.z;
+    posProj.w  = 1.0f;
     
     float4 posView = mul(posProj, InvertProjectionMatrix);
     return posView.xyz / posView.w;
 }
 
+
+
 float4 PS_Main(VS_OUT input) : SV_Target
 {
-    float3 viewRayEnd = ProjToView(input.uv);
-    float3 viewDir = normalize(viewRayEnd);
-    float3 origin = float3(0, 0, 0); 
+    float3 viewPos = ProjToView(input.ndcPos.xyz);
 
-    float3 accum = float3(0, 0, 0);
-
-    for (int i = 0; i < numSteps; ++i)
-    {
-        float t = stepSize * i;
-        float3 pos = origin + viewDir * t;
-
-        float3 sampleColor = float3(t * 0.01f, 0.0f, 0.0f);
-        accum += sampleColor;
-    }
-
-    return float4(saturate(accum), 1.0f);
+    return float4(1, 0, 0, 0);
 }
